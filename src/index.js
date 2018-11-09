9277f547311dc3225a5742830895914c80606e5b
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-09 17:31:40 -0600

    Restructure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
-import loadHeads from './components/LegoCharacter/heads';
-import loadTorsos from './components/LegoCharacter/torsos';
-import loadLegs from './components/LegoCharacter/legs';
-
-$('#click-button').on('click', () => {
-  $('#important-text').append('<p>YES I AM</p>');
-});
+import loadLegos from './helpers/legosLoader';
 
 const initializeApp = () => {
-  loadHeads();
-  loadTorsos();
-  loadLegs();
+  loadLegos();
 };
 
 initializeApp();
