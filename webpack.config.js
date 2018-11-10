eeb32590739b6b6c2260f06a2763e37f1477f436
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-10 16:25:15 -0600

    fixes favicon

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html')
+      template: path.resolve(__dirname, 'src/index.html'),
+      favicon: '.src/images/favicon.ico'
     }),
     extractScss
   ],
